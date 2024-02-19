50079aac737315e1a300e81e32c54a8bb57cdb58
Andsirpa <andsirpa94@gmail.com>
2024-02-19 16:42:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from './store';
+
+export default {
+  data() {
+    return {
+      store,
+      title: 'Ciao',
+    };
+  },
+
+
+};
+</script>
+
+<template>
+  <h1> {{ title }}</h1>
+</template>
+
+<style lang="scss">
+@use './assets/style/general.scss';
+</style>
